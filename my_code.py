bc4cc59d5568b8da4aca320346da152b0da25085
parkerS123 <23pschwantes@athenian.org>
2020-09-14 04:13:47 +0000

    finished!!!!

--- my_code.py
+++ my_code.py
@@ -1,19 +1,26 @@
-# Collaborators (including web sites where you got help: (enter none if you didn't need help)
+# Collaborators (including web sites where you got help: (enter none if you didn't need help) Faceprep.in
 #  
 
 def find_gcf(x,y):   # Do not change function name!
-    # User code goes here
+    
+    gcf = 1
 
+    while(gcf <= x and gcf <= y): 
+        if(x % gcf == 0 and y % gcf == 0):
+            answergcf = gcf
+        gcf = gcf + 1
 
-    return gcf
+    return answergcf
 
 
 if __name__ == '__main__':
     # Test your code with this first
     # Change the argument to try different values
-    print(find_gcf(6,9))
+    
 
     # After you are satisfied with your results, use input() to prompt the user for two values:
-    #x = int(input("Enter a number: "))
-    #y = int(input("Enter another number: "))
+    print("This program will tell you the greatest common factor of two numbers")
+    x = int(input("Enter a number: "))
+    y = int(input("Enter another number: "))
 
+    print(find_gcf(x,y))
