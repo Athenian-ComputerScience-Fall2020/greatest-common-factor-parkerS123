ef62e24a3c9e620edad0766adda435d9299f4b67
parkerS123 <23pschwantes@athenian.org>
2020-09-17 16:12:11 +0000

    collaborators

--- my_code.py
+++ my_code.py
@@ -1,5 +1,5 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help) 
-#  Faceprep.in(website)
+#  Faceprep.in (website) & mom
 
 def find_gcf(x,y):   # Do not change function name!
     
